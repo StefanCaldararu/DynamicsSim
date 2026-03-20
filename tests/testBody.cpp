d4f3d766187eec073f36ad33cef80c6a2f97d7fe
Stefan Caldararu <stefancaldararu@gmail.com>
2026-03-20 09:09:28 -0600

    :white_check_mark: add basic body test

---
+++ tests/testBody.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "dynamics/Body.hpp"
+
+using namespace Dynamics;
+
+TEST(BodyTest, Initialization) {
+    Eigen::Vector3f pos(1,2,3);
+    Eigen::Vector3f vel(0,1,0);
+
+    Body b(pos, vel, 5.0f, 1.0f);
+
+    EXPECT_EQ(b.getPosition(), pos);
+    EXPECT_EQ(b.getVelocity(), vel);
+    EXPECT_FLOAT_EQ(b.getMass(), 5.0f);
+}
