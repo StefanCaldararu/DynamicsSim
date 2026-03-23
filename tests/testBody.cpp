55b12b817368fe1ea6331779f18018f7ea9668c8
Stefan Caldararu <stefancaldararu@gmail.com>
2026-03-23 11:24:18 -0600

    :white_check_mark: add CR3BP unit tests

--- tests/testBody.cpp
+++ tests/testBody.cpp
@@ -1,13 +1,12 @@
 #include <gtest/gtest.h>
 #include "dynamics/Body.hpp"
 
-using namespace Dynamics;
 
 TEST(BodyTest, Initialization) {
     Eigen::Vector3f pos(1,2,3);
     Eigen::Vector3f vel(0,1,0);
 
-    Body b(pos, vel, 5.0f, 1.0f);
+    Dynamics::Body b(pos, vel, 5.0f, 1.0f);
 
     EXPECT_EQ(b.getPosition(), pos);
     EXPECT_EQ(b.getVelocity(), vel);
